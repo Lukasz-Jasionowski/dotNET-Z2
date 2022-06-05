7d9bf74a2931093791fccbff9f2774b6077d231d
Łukasz Jasionowski <85828127+Lukasz-Jasionowski@users.noreply.github.com>
2022-06-06 01:07:48 +0200

    Added some logic.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,169 @@ namespace dotNET_Z2
     /// </summary>
     public partial class MainWindow : Window
     {
+        char mar;
+        float num1, num2;
         public MainWindow()
         {
-            
+            InitializeComponent();
+
+        }
+
+        private void Procent_Click(object sender, RoutedEventArgs e)
+        {
+            mar = '%';
+            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            text.Text = "";
+        }
+
+        private void CE_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+        }
+
+        private void Cofnij_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JedenPrzezX_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DoKwadratu_Click(object sender, RoutedEventArgs e)
+        {
+            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            text.Text = (num1 * num1).ToString();
+            num1 *= num1;
+        }
+
+        private void Pierwiastek_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Podziel_Click(object sender, RoutedEventArgs e)
+        {
+            mar = '/';
+            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            text.Text = "";
+        }
+
+        private void Siedem_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "7";
+        }
+
+        private void Osiem_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "8";
+        }
+
+        private void Dziewiec_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "9";
+        }
+
+        private void Mnozenie_Click(object sender, RoutedEventArgs e)
+        {
+            mar = '*';
+            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            text.Text = "";
+        }
+
+        private void Cztery_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "4";
+        }
+
+        private void Piec_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "5";
+        }
+
+        private void Szesc_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "6";
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            mar = '-';
+            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            text.Text = "";
+        }
+
+        private void Jeden_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "1";
+        }
+
+        private void Dwa_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "2";
+        }
+
+        private void Trzy_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "3";
+        }
+
+        private void Dodac_Click(object sender, RoutedEventArgs e)
+        {
+            mar = '+';
+            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            text.Text = "";
+        }
+
+        private void ZmianaZnaku_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += "0";
+        }
+
+        private void Przecinek_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text += ".";
+        }
+
+        private void RownaSie_Click(object sender, RoutedEventArgs e)
+        {
+            num2 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            if (mar == '+')
+            {
+                text.Text = (num1 + num2).ToString();
+                num1 += num2;
+            }
+            else if (mar == '-')
+            {
+                text.Text = (num1 - num2).ToString();
+                num1 -= num2;
+            }
+            else if (mar == '*')
+            {
+                text.Text = (num1 * num2).ToString();
+                num1 *= num2;
+            }
+            else if (mar == '/')
+            {
+                text.Text = (num1 / num2).ToString();
+                num1 /= num2;
+            }
+            else if (mar == '%')
+            {
+                text.Text = (num1 % num2).ToString();
+                num1 %= num2;
+            }
         }
     }
 }
