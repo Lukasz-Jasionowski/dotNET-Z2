0cacf2f02cf21dc18d06a061f32768ef6422faba
Łukasz Jasionowski <85828127+Lukasz-Jasionowski@users.noreply.github.com>
2022-06-07 00:52:23 +0200

    The task has been completed.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,168 +23,221 @@ namespace dotNET_Z2
     public partial class MainWindow : Window
     {
         char mar;
-        float num1, num2;
+        double num1, num2;
         public MainWindow()
         {
             InitializeComponent();
 
         }
 
-        private void Procent_Click(object sender, RoutedEventArgs e)
+        private void Modulo_Click(object sender, RoutedEventArgs e)
         {
             mar = '%';
-            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            display.Text += "%";
             text.Text = "";
-            display.Text = "%";
         }
 
-        private void CE_Click(object sender, RoutedEventArgs e)
+        private void Silnia_Click(object sender, RoutedEventArgs e)
         {
-
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            double silnia = 1;
+            for (int i = 1; i <= num1; i++)
+            {
+                silnia *= i;
+            }
+            display.Text = num1 + "n!" + "=" + silnia.ToString();
         }
 
         private void C_Click(object sender, RoutedEventArgs e)
         {
             text.Text = "";
+            display.Text = "";
         }
 
-        private void Cofnij_Click(object sender, RoutedEventArgs e)
+        private void Floor_Click(object sender, RoutedEventArgs e)
         {
-
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            Obliczenia obliczenia = new Obliczenia();
+            num1 = obliczenia.Floor(num1);
+            text.Text = num1.ToString();
+            display.Text += "↓=" + num1.ToString();
         }
 
-        private void JedenPrzezX_Click(object sender, RoutedEventArgs e)
+        private void Ceiling_Click(object sender, RoutedEventArgs e)
         {
-
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            Obliczenia obliczenia = new Obliczenia();
+            num1 = obliczenia.Ceiling(num1);
+            text.Text = num1.ToString();
+            display.Text += "↑=" + num1.ToString();
         }
 
         private void DoKwadratu_Click(object sender, RoutedEventArgs e)
         {
-            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
             text.Text = (num1 * num1).ToString();
+            display.Text += num1.ToString() + "*" + num1.ToString();
             num1 *= num1;
         }
 
         private void Pierwiastek_Click(object sender, RoutedEventArgs e)
         {
-
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            Obliczenia obliczenia = new Obliczenia();
+            num1 = obliczenia.Sqrt(num1);
+            text.Text = num1.ToString();
+            display.Text += "√" + "=" + num1.ToString();
         }
 
         private void Podziel_Click(object sender, RoutedEventArgs e)
         {
             mar = '/';
-            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            display.Text += "/";
             text.Text = "";
         }
 
         private void Siedem_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "7";
+            display.Text += "7";
         }
 
         private void Osiem_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "8";
+            display.Text += "8";
         }
 
         private void Dziewiec_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "9";
+            display.Text += "9";
         }
 
         private void Mnozenie_Click(object sender, RoutedEventArgs e)
         {
             mar = '*';
-            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
             text.Text = "";
+            display.Text += "*";
         }
 
         private void Cztery_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "4";
+            display.Text += "4";
         }
 
         private void Piec_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "5";
+            display.Text += "5";
         }
 
         private void Szesc_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "6";
+            display.Text += "6";
         }
 
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
             mar = '-';
-            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
             text.Text = "";
+            display.Text += "-";
         }
 
         private void Jeden_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "1";
+            display.Text += "1";
         }
 
         private void Dwa_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "2";
+            display.Text += "2";
         }
 
         private void Trzy_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "3";
+            display.Text += "3";
         }
 
         private void Dodac_Click(object sender, RoutedEventArgs e)
         {
             mar = '+';
-            num1 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
             text.Text = "";
+            display.Text += "+";
         }
 
         private void ZmianaZnaku_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!text.Text.Contains("-"))
+            {
+                num1 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+                text.Text += "-";
+                display.Text = (num1 * -1).ToString();
+                text.Text = (num1 * -1).ToString();
+                num1 *= -1;
+            }
+            else
+            {
+                text.Text = (num1 * -1).ToString();
+                num1 *= -1;
+                display.Text = (num1).ToString();
+            }
         }
 
         private void Zero_Click(object sender, RoutedEventArgs e)
         {
             text.Text += "0";
+            display.Text += "0";
         }
 
         private void Przecinek_Click(object sender, RoutedEventArgs e)
         {
             text.Text += ".";
+            display.Text += ",";
         }
 
         private void RownaSie_Click(object sender, RoutedEventArgs e)
         {
-            num2 = float.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
+            num2 = double.Parse(text.Text, CultureInfo.InvariantCulture.NumberFormat);
             if (mar == '+')
             {
                 text.Text = (num1 + num2).ToString();
                 num1 += num2;
+                display.Text += "=" + num1;
             }
             else if (mar == '-')
             {
                 text.Text = (num1 - num2).ToString();
                 num1 -= num2;
+                display.Text += "=" + num1;
             }
             else if (mar == '*')
             {
                 text.Text = (num1 * num2).ToString();
                 num1 *= num2;
+                display.Text += "=" + num1;
             }
             else if (mar == '/')
             {
                 text.Text = (num1 / num2).ToString();
                 num1 /= num2;
+                display.Text += "=" + num1;
             }
             else if (mar == '%')
             {
                 text.Text = (num1 % num2).ToString();
                 num1 %= num2;
+                display.Text += "=" + num1;
             }
         }
     }
